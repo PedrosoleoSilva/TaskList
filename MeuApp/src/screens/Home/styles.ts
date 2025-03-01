ac8da1a2c15da95275629afdf063c14cfa9933d5
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2025-03-01 17:30:08 -0300

    criando tela home

--- MeuApp/src/screens/Home/styles.ts
+++ MeuApp/src/screens/Home/styles.ts
@@ -12,6 +12,19 @@ export const styles = StyleSheet.create({
         marginTop: 48
     },
     dateEvent: {
-        fontSize: 16
+        fontSize: 16,
+        color: '#000'
+    },
+    textInput: {
+        height: 56,
+        backgroundColor: '#17e610',
+        borderRadius: 5,
+        padding: 16,
+        fontSize: 16,
+        marginRight: 12,
+        fontWeight: 'bold'    
+    },
+    form : {
+        
     }
 });
